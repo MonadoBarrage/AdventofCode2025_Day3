f0228bc5de2d0cc97e426f1fed1d98abe7b46809
MonadoBarrage <chenkatti@tutanota.com>
2025-12-31 02:39:26 -0500

    Added part 2 to day 3

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let mut sum = 0;
     for line in file_string.lines() {
         // println!("{}", line);
-        sum += find_highest_number(&String::from(line));
+        sum += find_highest_number_part2(&String::from(line));
     }
     println!("Sum: {}", sum);
 }
@@ -33,4 +33,41 @@ fn find_highest_number(number_string: &String) -> u64{
     }
 
     return highest_number;
+}
+
+fn find_highest_number_part2(number_string: &String) -> u64{
+    let length = number_string.len();
+    if length < 12 {return 0; }
+    if length == 12 { return number_string.parse::<u64>().unwrap(); }
+
+    let mut highest_number : u64 = 0;
+
+
+    let split_numbers = number_string.chars().collect::<Vec<char>>();
+
+
+    let mut position_tracker = 12;
+
+    let mut counter = 0;
+    let mut current_highest_int :u64 = 0;
+
+    while position_tracker > 0 {
+
+        for i in counter..(length-position_tracker+1){
+            let compared_num = split_numbers[i].to_digit(10).unwrap() as u64;
+            if compared_num > current_highest_int{
+                counter = i;
+                current_highest_int = compared_num;
+            }
+        }
+        highest_number += current_highest_int*(10_u64.pow(position_tracker as u32 -1));
+
+        counter += 1;
+        current_highest_int = 0;
+
+        position_tracker -= 1;
+    }
+    println!("Highest number: {}", highest_number);
+
+    highest_number
 }
